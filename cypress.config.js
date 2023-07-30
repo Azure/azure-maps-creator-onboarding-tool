9435c537ec19fca87d70cb70b867c729d12239d1
Artur Yapparov <v-aryapparov@microsoft.com>
2023-07-30 17:30:29 +0300

    fixed cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,36 @@
 const { defineConfig } = require('cypress');
+const zip = require('@zip.js/zip.js');
 
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        async validateManifestFromZip({ drawingPackage, expectedManifest }) {
+          const zipFileReader = new zip.Data64URIReader(drawingPackage);
+          const zipReader = new zip.ZipReader(zipFileReader);
+          let downloadedManifest = {};
+          const entries = await zipReader.getEntries();
+
+          for (let i = 0; i < entries.length; i++) {
+            const file = entries[i];
+            if (file.filename.toLowerCase() === 'manifest.json') {
+              const data = await file.getData(new zip.BlobWriter());
+              downloadedManifest = JSON.parse(await data.text());
+            }
+          }
+          if (JSON.stringify(downloadedManifest) !== JSON.stringify(expectedManifest)) {
+            throw new Error(JSON.stringify({
+              error: 'Manifest does not match.',
+              downloadedManifest,
+              expectedManifest,
+            }));
+          }
+
+          return null;
+        }
+      });
     },
   },
 });
