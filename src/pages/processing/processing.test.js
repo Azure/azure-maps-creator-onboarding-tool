8fc83d353598063756418d1798079e47708fc46c
nizetic <pjero.nizetic@gmail.com>
2023-10-16 17:29:01 +0200

    Remove countdown test

--- src/pages/processing/processing.test.js
+++ src/pages/processing/processing.test.js
@@ -1,9 +1,7 @@
-import { act, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { useResponseStore, useUserStore } from 'common/store';
-import ProcessingPage, { REFRESH_INTERVAL } from './processing';
+import ProcessingPage from './processing';
 
-const refreshRateMs = REFRESH_INTERVAL * 1000;
-const requestFrequency = 1;
 const mockT = jest.fn();
 const mockNavigate = jest.fn();
 
@@ -28,37 +26,6 @@ describe('ProcessingPage', () => {
 
   it('should match snapshot', () => {
     expect(render(<ProcessingPage />)).toMatchSnapshot();
-  });
-
-  it('should countdown', async () => {
-    const state = useResponseStore.getState();
-    const spy = jest.spyOn(state, 'refreshStatus');
-    jest.useFakeTimers();
-    render(<ProcessingPage />);
-    for (let i = 0; i < requestFrequency; i++) {
-      expect(mockT).toHaveBeenCalledWith('processing.last.checked', {
-        seconds: i,
-      });
-      act(() => {
-        jest.advanceTimersByTime(refreshRateMs);
-      });
-    }
-
-    expect(spy).toHaveBeenCalledWith('subscriptionKey');
-    expect(spy).toHaveBeenCalledTimes(1);
-
-    for (let i = 0; i < requestFrequency * 10; i++) {
-      expect(mockT).toHaveBeenCalledWith('processing.last.checked', {
-        seconds: i % 10,
-      });
-      act(() => {
-        jest.advanceTimersByTime(refreshRateMs);
-      });
-    }
-
-    expect(spy).toHaveBeenCalledTimes(11);
-
-    jest.useRealTimers();
   });
 
   it('should navigate to create manifest page in case of error', () => {
