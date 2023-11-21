b2ba2c8cc184eb735468d08c7397c8d3a1af3bc8
Pjero Nizetic <pjero@janeasystems.com>
2023-11-21 16:07:08 +0100

    Small rewrites

--- src/common/store/index.js
+++ src/common/store/index.js
@@ -1,9 +1,9 @@
-export { getDomain, useUserStore } from './user.store';
-export { useResponseStore } from './response.store';
-export { progressBarSteps, progressBarStepsByKey, useCompletedSteps, useProgressBarStore } from './progress-bar-steps';
+export { conversionSteps, useConversionStore } from './conversion.store';
 export { useDissolvedExterior, useGeometryStore } from './geometry.store';
 export { useLayersStore } from './layers.store';
 export { useLevelsStore } from './levels.store';
-export { useReviewManifestJson, useReviewManifestStore } from './review-manifest.store';
-export { useConversionStore, conversionSteps } from './conversion.store';
+export { progressBarSteps, progressBarStepsByKey, useCompletedSteps, useProgressBarStore } from './progress-bar-steps';
 export { resetStores } from './reset';
+export { LRO_STATUS, useResponseStore } from './response.store';
+export { useReviewManifestJson, useReviewManifestStore } from './review-manifest.store';
+export { getDomain, useUserStore } from './user.store';
--- src/components/route/route.js
+++ src/components/route/route.js
@@ -1,30 +1,35 @@
-import { useTranslation } from 'react-i18next';
-import PropTypes from 'prop-types';
 import { cx } from '@emotion/css';
+import { PATHS } from 'common';
+import { LRO_STATUS, progressBarSteps, useResponseStore, useUserStore } from 'common/store';
+import PropTypes from 'prop-types';
 import { useEffect, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useLocation, useNavigate } from 'react-router-dom';
-
 import BreadCrumbNav from '../bread-crumb-nav/bread-crumb-nav';
 import Footer from '../footer/footer';
-import TopBar from '../top-bar/top-bar';
 import ProgressBar from '../progress-bar/progress-bar';
-import { routeStyle, footerPadding } from './route.style';
-import { useResponseStore, LRO_STATUS } from 'common/store/response.store';
-import { PATHS } from 'common';
-import { progressBarSteps } from 'common/store';
+import TopBar from '../top-bar/top-bar';
+import { footerPadding, routeStyle } from './route.style';
 
 const responseStoreSelector = s => s.lroStatus;
+const userStoreSelector = s => s.subscriptionKey;
 
 const Route = ({ title, component: Component, dataRequired }) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const lroStatus = useResponseStore(responseStoreSelector);
+  const { pathname: currentPath } = useLocation();
 
-  const shouldShowFooter = useMemo(
-    () => progressBarSteps.findIndex(route => route.href === pathname) !== -1,
-    [pathname]
-  );
+  const lroStatus = useResponseStore(responseStoreSelector);
+  const subKey = useUserStore(userStoreSelector);
+
+  const shouldShowFooter = useMemo(() => {
+    return progressBarSteps.findIndex(route => route.href === currentPath) !== -1;
+  }, [currentPath]);
+
+  useEffect(() => {
+    // Redirect to index if there is no subscription key
+    if (!subKey) navigate(PATHS.INDEX, { replace: true });
+  }, [navigate, currentPath, subKey]);
 
   useEffect(() => {
     if (dataRequired && lroStatus !== LRO_STATUS.SUCCEEDED && lroStatus !== LRO_STATUS.FETCHING_DATA) {
