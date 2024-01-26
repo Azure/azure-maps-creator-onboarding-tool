a0b33343fc66f01688cc904310acf37301831c52
Pjero Nizetic <pjero@janeasystems.com>
2024-01-26 16:45:49 +0100

    Migrate to createRoot

--- src/pages/georeference/control/controlClass.js
+++ src/pages/georeference/control/controlClass.js
@@ -1,5 +1,4 @@
-import { render } from 'react-dom';
-
+import { createRoot } from 'react-dom/client';
 import Control from './control';
 
 class GeoreferenceControl {
@@ -7,7 +6,10 @@ class GeoreferenceControl {
     this.map = map;
     this.container = document.createElement('div');
     this.container.classList.add('azure-maps-control-container');
-    render(<Control map={this.map} />, this.container);
+
+    const root = createRoot(this.container);
+
+    root.render(<Control map={this.map} />);
     return this.container;
   }
 
