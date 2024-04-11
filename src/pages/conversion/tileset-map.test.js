a85d9962f569d68f128ceb752536eeebcc6545bb
Pjero Nizetic <pjero@janeasystems.com>
2024-04-11 19:47:44 +0200

    Upgrade azure maps dependency to use latest base maps

--- src/pages/conversion/tileset-map.test.js
+++ src/pages/conversion/tileset-map.test.js
@@ -4,14 +4,15 @@ import TilesetMap from './tileset-map';
 
 jest.mock('azure-maps-control', () => ({
   control: {
-    StyleControl: function() {},
-    ZoomControl: function() {},
+    StyleControl: function () {},
+    ZoomControl: function () {},
   },
   Map: jest.fn(),
 }));
 jest.mock('common/store', () => ({
   useConversionStore: () => ['map-config-id', [0, 1, 2, 3]],
   useUserStore: () => ['US', 'key-subscription'],
+  getDomain: () => 'us.atlas.microsoft.com',
 }));
 
 describe('Tileset map', () => {
@@ -19,22 +20,20 @@ describe('Tileset map', () => {
     azMapsControl.Map.mockImplementation(() => ({
       controls: {
         add: () => {},
-      }
+      },
     }));
   });
   it('should render map', () => {
     render(<TilesetMap />);
     const constructorSpy = jest.spyOn(azMapsControl, 'Map');
-    expect(constructorSpy).toBeCalledWith(
-      'azure-maps-container',
-      {
-        bounds: [0, 1, 2, 3],
-        domain: 'https://us.atlas.microsoft.com',
-        language: 'en-US',
-        mapConfiguration: 'map-config-id',
-        staticAssetsDomain: 'https://us.atlas.microsoft.com',
-        styleAPIVersion: '2023-03-01-preview',
-        subscriptionKey: 'key-subscription',
-      });
+    expect(constructorSpy).toBeCalledWith('azure-maps-container', {
+      bounds: [0, 1, 2, 3],
+      domain: 'us.atlas.microsoft.com',
+      language: 'en-US',
+      mapConfiguration: 'map-config-id',
+      staticAssetsDomain: 'us.atlas.microsoft.com',
+      styleAPIVersion: '2023-03-01-preview',
+      subscriptionKey: 'key-subscription',
+    });
   });
 });
